c2d7e401acbbecd09294d158edb08aa225801a76
vinodh1988 <k.c.vinodh@hotmail.com>
2022-07-04 15:25:59 +0530

    first-commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const fs=require("fs")
+const second=require("./second")
 console.log("First Node JS Application-> second run")
+console.log(second.people);
+second.hello();
 
 fs.readFile("package.json","utf-8",function(err,data){
     if(err)
---
+++ second.js
@@ -0,0 +1,7 @@
+const names=["Ram","Raghav","Ramesh","Rahul"]
+
+const fun=function(){
+    console.log("Hello!!!")
+}
+
+module.exports={people:names, hello:fun}
