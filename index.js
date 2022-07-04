c6363d694fbe4b21708083f6d284731a5ddca4d5
vinodh1988 <k.c.vinodh@hotmail.com>
2022-07-04 15:53:18 +0530

    new-commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express=require("express");
 var app=express();
+var path=require("path");
 
 app.get("/",function(request,response){
     response.send("Node JS is working");
@@ -7,6 +8,10 @@ app.get("/",function(request,response){
 
 app.get("/greet/:name",function(request,response){
     response.send("Hello!!!"+request.params.name);
+})
+
+app.get("/home",function(request,response){
+    response.sendFile(path.join(__dirname,"public/index.html"));
 })
 
 app.listen("3000",function(){
